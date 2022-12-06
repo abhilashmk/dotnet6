aaa02bc47fc1e88166223750f4e7713df7f4e810
abhilashmk <abhilashkonnur1@gmail.com>
2022-12-06 16:44:38 +0530

    Update Program.cs
    Test change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+//test commit
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
